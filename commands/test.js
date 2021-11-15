96b49bbc071f9ec898ef4e9ad6b2334f4b358f3d
pawaret <62021393@up.ac.th>
2021-11-15 12:51:36 +0700

    Hotfix

--- commands/test.js
+++ commands/test.js
@@ -5,7 +5,7 @@ const separator = require("number-separator");
 module.exports = {
   data: new SlashCommandBuilder().setName("test").setDescription("test btc"),
   async execute(interaction) {
-    interaction.reply({ content: "asd", ephemeral: true });
+    interaction.reply({ content: "test", ephemeral: true });
   },
 };
 
