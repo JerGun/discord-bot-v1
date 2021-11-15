90fb110a5b49c8a8ac726a930efedf9dae302f7d
pawaret <62021393@up.ac.th>
2021-11-15 13:44:47 +0700

    Update help command

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
@@ -5,7 +6,19 @@ module.exports = {
     .setName("help")
     .setDescription("Display the help menu"),
   async execute(interaction) {
+    const embed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setDescription(
+        "**$help** - Display the help menu\n\n\
+  ***Cryptocurrency Price***\n\
+  **$btc** - Bitcoin Price\n\
+  **$eth** - Ethereum Price\n\
+  **$bnb** - Binance Coin Price\n\
+  **$kub** - Bitkub Coin Price\n\
+  **$ccar** - CryptoCars Price\n\
+  **$lumi** - LUMI Price"
+      );
     await interaction.deferReply({ ephemeral: true });
-    interaction.editReply({ content: "Coming Soon!", ephemeral: true });
+    interaction.editReply({ embeds: [ embed ] , ephemeral: true });
   },
 };
