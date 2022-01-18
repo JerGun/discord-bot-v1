a2f9f8b863605bc0c879a03006e5046cbd6fc884
pawaret <62021393@up.ac.th>
2022-01-18 22:09:55 +0700

    Update project due

--- commands/remind.js
+++ commands/remind.js
@@ -1,13 +1,12 @@
 const { MessageEmbed } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { exec } = require("child_process");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("project")
     .setDescription("Shows the number of days remaining to do the project."),
   async execute(interaction) {
-    let finalDate = new Date("Mar 23 2022 00:00:00").getTime();
+    let finalDate = new Date("Mar 22 2022 00:00:00").getTime();
     let today = new Date().getTime();
     let remaining = finalDate - today;
     let remainingDays = Math.floor(remaining / (1000 * 60 * 60 * 24));
